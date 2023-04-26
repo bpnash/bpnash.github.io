bcbe3842c8dbc16636b1ec2d46004a73adc6093c
bpnash <bpnash65@gmail.com>
2023-04-26 14:37:40 +0100

    First commit to GitHub Pages site

---
+++ README.md
@@ -0,0 +1,6 @@
+# bpnash site
+
+This site is beginning my journey to creating a web presence
+
+The initial first step is to create a holding page to test the GitHub Pages repo and site have been set up correctly.
+The next step is to connect to my custom domain
---
+++ js/index.js
@@ -0,0 +1,13 @@
+// This file is for the site interaction and is not part of the practicals
+const navbar = document.getElementById('navbar');
+let scrolled = false;
+
+window.onscroll = () => {
+  if (window.pageYOffset > 100) {
+    navbar.classList.remove('top');
+    scrolled = true;
+  } else {
+    navbar.classList.add('top');
+    scrolled = false;
+  }
+};
